e906ab33d23899736ea2aa5c7b2427484236809d
liyuntao <liyuntao58607@gmail.com>
2020-12-01 17:30:47 +0800

    first commit

---
+++ examples/day01.rs
@@ -0,0 +1,38 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+
+pub fn read_input(file_name: &str) -> Result<HashSet<i64>, Error> {
+    let path = format!("./input/{}", file_name);
+    let br = BufReader::new(File::open(path)?);
+    br.lines()
+        .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
+        .collect()
+}
+
+pub fn two_sum(numbers: &HashSet<i64>, exclude: i64, sum_eq: i64) -> i64 {
+    for i in numbers.iter() {
+        if *i != exclude && numbers.contains(&(sum_eq - i)) {
+            return i * (sum_eq - i);
+        }
+    }
+    return 0;
+}
+
+fn main() {
+    let numbers = read_input("day01.txt").unwrap();
+    for i in numbers.iter() {
+        if numbers.contains(&(2020 - i)) {
+            println!("result of q01 is {}", i * (2020 - i));
+            break;
+        }
+    }
+
+    for i in numbers.iter() {
+        let two_sum_res = two_sum(&numbers, i.clone(), 2020 - i);
+        if two_sum_res > 0 {
+            println!("result of q02 is {}", i * two_sum_res);
+            break;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,27 @@
+## Examples of how to use adventofcode2020
+
+This directory contains a number of examples showcasing the solutions of [Advent Of Code 2020](https://adventofcode.com/2020).
+
+All solutions can be executed with:
+
+```
+cargo run --example $name
+
+# e.g. 
+cargo run --example day01
+```
+
+
+
+```
+# you may have to put your input into ./input dir
+
+# if you wanna make it run faster:
+cargo run --example day01 --release
+
+# measure execution time
+time cargo run --example day01 --release
+```
+
+
+[中文版解题思路](https://zhuanlan.zhihu.com/p/51992007)
